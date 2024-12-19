7e4564d1583af7ce516eee50d3882df14b784363
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2024-12-19 15:04:57 +1300

    improve graceful shutdown

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,10 @@ echo "[entrypoint] Removing /tmp/.X0-lock..."
 rm -f /tmp/.X0-lock 2>&1
 
 echo "[entrypoint] Re(setting) invite seed..."
-echo -n "${inviteseed}" >  ${p}/Config/InviteSeed.cfg
+echo -n "${inviteseed}" > ${p}/Config/InviteSeed.cfg
+
+echo "[entrypoint] Starting Xvfb"
+Xvfb :0 -screen 0 1280x1024x24 &
 
 echo "[entrypoint] Launching wine64 Return to Moria..."
-exec xvfb-run wine64 "${s}/MoriaServer.exe" 2>&1
+exec env DISPLAY=:0.0 wine64 "${s}/Moria/Binaries/Win64/MoriaServer-Win64-Shipping.exe" Moria 2>&1
